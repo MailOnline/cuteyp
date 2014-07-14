3cf9b14a9c57f3b374c987cad9b8059163347e9c
Tom Howe <tom.howe@mailonline.co.uk>
2014-07-14 17:47:54 +0100

    tom: more or less working, versions next

--- stomp2express.js
+++ stomp2express.js
@@ -1,57 +1,91 @@
 var Response = require('./lib/response')
-    ,stompit = require('stompit');
+    , stompit = require('stompit')
+    , _ = require('lodash')
+    , express = require('express')
+    , http = require('http')
+    , FakeSocket = require("./lib/fakesocket");
 
-
-module.exports = function(app, stompOpts, path, filterFn) {
-
+module.exports = function (app, stompOpts, subscriptionPath, filterFn) {
     if (!stompOpts) {
         return console.log("No stomp opts provided, not connecting");
     }
     var connections = new stompit.ConnectFailover([stompOpts]);
-
     var channel = new stompit.ChannelFactory(connections);
+    console.log("Subscribing to", subscriptionPath);
 
-    channel.subscribe(path, function(error, message) {
-
+    channel.subscribe(subscriptionPath, function (error, message) {
         if (error) {
             console.error('subscribe error ' + error.message);
             return;
         }
-
-        message.readString('utf8', function(error, body) {
-
+        message.readString('utf8', function (error, body) {
             if (error) {
                 console.error('read message error ' + error.message);
-                return;
+                return message.ack();
             }
 
-            console.log('received message: ' + body);
             var parsed = JSON.parse(body);
-            var replyTo = parsed.replyTo;
+            console.log("Received request ", parsed.url);
 
+            var replyTo = parsed.replyTo;
+            var correlationId = parsed.correlationId;
             if (filterFn) {
                 if (!filterFn(parsed)) {
                     console.error("Ignoring message, failed filter");
                     return;
                 }
             }
+            var headers = parsed.headers;
+            headers.correlationId = correlationId;
 
-            var req = {
+            var fakeSocket = new FakeSocket();
+
+            var req = new http.IncomingMessage(fakeSocket);
+
+            _.extend(req, {
+                connection: {},   //to allow session to not break
                 headers: parsed.headers,
                 url: parsed.url,
                 method: parsed.method
+            });
+
+            var baseResp = {
+                __filename: __filename,
+                __script: process.argv[1],
+                correlationId: correlationId
             };
 
-            var res = new Response({
-                sendFn: function(resp) {
-                    console.log("returning resp", resp);
-                    channel.send(replyTo, JSON.stringify(resp));
+            var res = new Response(req, {
+                endFn: function (body) {
+                    res.emit('header');
+
+                    _.extend(baseResp, {
+                        headers: res._headers,
+                        code: res.statusCode,
+                    });
+
+                    if (body) baseResp.body = body;
+
+                    channel.send(replyTo, JSON.stringify(baseResp));
+
+                    message.ack();
                 }
+            });
+
+            res.on('close', function() {
+                console.log("Response closed");
+            });
+
+            res.on('finish', function() {
+                console.log("Response finished");
             });
 
             app.handle(req, res);
 
-            message.ack();
+            req.emit('data', parsed.body);
+            req.emit('end');
+
+
         });
     });
 };
