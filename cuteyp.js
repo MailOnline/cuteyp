d4e75c604679667bb66c9ea2a85230e51f535a8e
Tom Howe <tom.howe@mailonline.co.uk>
2014-07-15 14:44:04 +0100

    tom: support redis too

--- cuteyp.js
+++ cuteyp.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var Stomp = require('./lib/stomp')
-    , toHttp = require('./lib/tohttp')
+var
+     toHttp = require('./lib/tohttp')
     , fromHttp = require('./lib/fromhttp')
+    , Stomp = require('./lib/stomp')
+    , Redis = require('./lib/redis')
     ;
 
 var exports = module.exports;
 exports.fromHttp = fromHttp;
 exports.toHttp = toHttp;
 exports.stomp = Stomp;
+exports.redis = Redis;
