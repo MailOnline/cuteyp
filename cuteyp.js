6ad60489e3a41574af88f834764f089cd77bf06a
Tom Howe <tom.howe@mailonline.co.uk>
2014-09-23 15:00:00 +0100

    tom: support for redis queues

--- cuteyp.js
+++ cuteyp.js
@@ -5,6 +5,7 @@ var
     , fromHttp = require('./lib/fromhttp')
     , Stomp = require('./lib/stomp')
     , Redis = require('./lib/redis')
+    , RedisList = require('./lib/redislist')
     ;
 
 var exports = module.exports;
@@ -12,3 +13,4 @@ exports.fromHttp = fromHttp;
 exports.toHttp = toHttp;
 exports.stomp = Stomp;
 exports.redis = Redis;
+exports.redislist = RedisList;
